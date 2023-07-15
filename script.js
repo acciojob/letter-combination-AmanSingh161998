48fe6b3e5ea795fe82910fa85abac9e325f18b4a
AmanSingh161998 <122721757+AmanSingh161998@users.noreply.github.com>
2023-07-15 20:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function letterCombinations(input) {
   //Complete the function
 
 	let table ={
-		2:"abc",
-		3:"def",
-		4:"ghi",
-		5:"jkl",
-		6:"mno",
-		7:"pqrs",
-		8:"tuv",
-		9:"wxyz",
+	    "2":"abc",
+		"3":"def",
+		"4":"ghi",
+		"5":"jkl",
+		"6":"mno",
+		"7":"pqrs",
+		"8":"tuv",
+		"9":"wxyz",
 	}
 	let solution=[];
-	function solve( currentIndex,calculatedString){
+	function solve(currentIndex,calculatedString){
 		if(input.length<=currentIndex){
 			solution.push(calculatedString);
 			return;
